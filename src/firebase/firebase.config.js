faf80d99a48b7a588cc455fcaf51480a044d9c9c
Remon Tripura <remontripura045@gmail.com>
2023-05-04 10:41:04 +0600

    environment setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDi9-xE74t6eWPqZSDbNWwwSUrGl8gI1b8",
-  authDomain: "chef-recipe-301c5.firebaseapp.com",
-  projectId: "chef-recipe-301c5",
-  storageBucket: "chef-recipe-301c5.appspot.com",
-  messagingSenderId: "707193168577",
-  appId: "1:707193168577:web:08dde24fb9e249a9daaa40"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
